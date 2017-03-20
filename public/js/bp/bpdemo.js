e7035502c91b95425f0666513cbedbf595662980
Yu Li <li.yu@dianrong.com>
2017-03-20 10:09:57 +0800

    update

--- public/js/bp/bpdemo.js
+++ public/js/bp/bpdemo.js
@@ -32,14 +32,12 @@ function squareErr(target, current) {
 
 //前向传播
 function forward() {
-    for(var x=0;x<w.length;x++){
-        for(var y=0;y<w[x].length;y++){
-            h[0] = sigmoid(i[0] * w[0][y] + i[1] * w[0][y+1] + b[0]);
-        }
-    }
-    
-	h[0] = sigmoid(i[0] * w[0][0] + i[1] * w[0][1] + b[0]);
-	h[1] = sigmoid(i[0] * w[0][2] + i[1] * w[0][3] + b[0]);
+
+	for (var x = 0; x < i.length; x++) {
+		h[0] = sigmoid(i[0] * w[0][0] + i[1] * w[0][1] + b[0]);
+		h[1] = sigmoid(i[0] * w[0][2] + i[1] * w[0][3] + b[0]);
+
+	}
 
 	o[0] = sigmoid(h[0] * w[1][0] + h[1] * w[1][1] + b[1]);
 	o[1] = sigmoid(h[0] * w[1][2] + h[1] * w[1][3] + b[1]);
@@ -56,13 +54,13 @@ function totalError() {
 //反向传播第一层
 function backward1() {
 	var newWeight = [];
-    for(var y=0;y<t.length;y++){
-        for (var x = 0; x < h.length; x++) {
-            newWeight[x] = -1 * (t[y] - o[y]) * o[y] * (1 - o[y]) * h[x];
-            newWeight[x] = w[1][x+y*(2)] - lr * newWeight[x];
-            console.info("权重变化: "+w[1][x+y*(2)] + "=>" + newWeight[x]);
-        }
-    }	
+	for (var y = 0; y < t.length; y++) {
+		for (var x = 0; x < h.length; x++) {
+			newWeight[x] = -1 * (t[y] - o[y]) * o[y] * (1 - o[y]) * h[x];
+			newWeight[x] = w[1][x + y * (2)] - lr * newWeight[x];
+			console.info("权重变化: " + w[1][x + y * (2)] + "=>" + newWeight[x]);
+		}
+	}
 }
 
 forward();
